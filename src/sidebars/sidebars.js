344a8945178679839b18b62312cdf5d2b748dbd9
Ivaylo Nakov <i.nakov18@gmail.com>
2023-07-08 18:30:23 +0200

    sidebar basic expanidn functoinality

--- src/sidebars/sidebars.js
+++ src/sidebars/sidebars.js
@@ -5,4 +5,17 @@
   tooltipTriggerList.forEach(tooltipTriggerEl => {
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
+
+    document.querySelectorAll("#sidebar-narrow a[data-bs-original-title]").forEach(el=>el.addEventListener('click',()=>{
+        document.getElementById('sidebar-wide').style.visibility='visible'
+        document.getElementById('sidebar-narrow').style.visibility='collapse'
+        document.getElementById('sidebar-expanded').style.visibility='collapse'
+    }))
+
+    document.querySelector("#sidebar-wide a[data-bs-original-title='Orders']").addEventListener('click',()=>{
+        document.getElementById('sidebar-wide').style.visibility='collapse'
+        document.getElementById('sidebar-narrow').style.visibility='visible'
+        document.getElementById('sidebar-expanded').style.visibility='visible'
+    })
+
 })()
