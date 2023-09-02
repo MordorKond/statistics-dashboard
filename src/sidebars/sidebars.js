3c598d71b0ab3c0cedf377b709a072740d920652
Ivaylo Nakov <i.nakov18@gmail.com>
2023-09-02 22:42:13 +0200

    updated the on hover

--- src/sidebars/sidebars.js
+++ src/sidebars/sidebars.js
@@ -6,16 +6,44 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 
-    document.querySelectorAll("#sidebar-narrow a[data-bs-original-title]").forEach(el=>el.addEventListener('click',()=>{
-        document.getElementById('sidebar-wide').style.visibility='visible'
-        document.getElementById('sidebar-narrow').style.visibility='collapse'
-        document.getElementById('sidebar-expanded').style.visibility='collapse'
+    document.querySelectorAll("a[data-bs-original-title]").forEach(el=>el.addEventListener('mouseenter',(e)=>{
+        const option =e.target.attributes['data-bs-original-title'].value.trim().toLowerCase() 
+        let sidebar = document.getElementById('sidebar-expanded-'+option)
+        let overlay = document.getElementById('main-pane-overlay')
+        if(!sidebar) {
+            document.querySelectorAll(".sidebar-expanded").forEach(el=>el.style.visibility='collapse')
+            overlay.style.visibility='collapse'
+        }
+        // const visibilityState=(sidebar.style.visibility=="collapse")?"visible":"collapse"
+        // sidebar.style.visibility=visibilityState
+        sidebar.style.visibility="visible"
+        overlay.style.visibility="visible"
+        console.log(e.target.innerText.trim().toLowerCase())
     }))
 
-    document.querySelector("#sidebar-wide a[data-bs-original-title='Orders']").addEventListener('click',()=>{
-        document.getElementById('sidebar-wide').style.visibility='collapse'
-        document.getElementById('sidebar-narrow').style.visibility='visible'
-        document.getElementById('sidebar-expanded').style.visibility='visible'
+    /* document.querySelectorAll("a[data-bs-original-title]").forEach(el=>el.addEventListener('mouseout',(e)=>{
+        const option =e.target.innerText.trim().toLowerCase() 
+        let sidebar = document.getElementById('sidebar-expanded-'+option)
+        if(!sidebar)return;
+        // const visibilityState=(sidebar.style.visibility=="collapse")?"visible":"collapse"
+        // sidebar.style.visibility=visibilityState
+        sidebar.style.visibility="collapse"
+        console.log(e.target.innerText.trim().toLowerCase())
+    })) */
+
+    document.querySelector(".main-pane-overlay").addEventListener('mouseenter',()=>{
+        document.querySelectorAll(".sidebar-expanded").forEach(el=>el.style.visibility='collapse')
+        let overlay = document.getElementById('main-pane-overlay')
+        overlay.style.visibility='collapse'
+        // document.getElementById('sidebar-wide').style.visibility='collapse'
+        // document.getElementById('sidebar-narrow').style.visibility='visible'
+        // document.getElementById('sidebar-expanded').style.visibility='visible'
     })
+
+    /* document.querySelector("a[data-bs-original-title='Orders']").addEventListener('click',()=>{
+        // document.getElementById('sidebar-wide').style.visibility='collapse'
+        // document.getElementById('sidebar-narrow').style.visibility='visible'
+        document.getElementById('sidebar-expanded').style.visibility='visible'
+    }) */
 
 })()
